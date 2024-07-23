9621968d92be6160b21438295468c9925b5285b3
graceful-01 <leeyb0330@sunmoon.ac.kr>
2024-07-23 11:36:58 +0900

    멘토님이 주신 랭체인과 올라마, 그리고 RAG 포함해서 답변 나온거

--- chatbot.py
+++ chatbot.py
@@ -1,8 +1,8 @@
 import transformers
 import torch
 from transformers import AutoModelForCausalLM, BitsAndBytesConfig, AutoTokenizer
-
-model_id = "yanolja/EEVE-Korean-Instruct-10.8B-v1.0"
+import time
+model_id = "yanolja/EEVE-Korean-2.8B-v1.0"
 
 # GPU 사용 가능 여부 확인
 device = "cuda" if torch.cuda.is_available() else "cpu"
@@ -33,8 +33,9 @@ pipeline = transformers.pipeline(
 # 모델을 평가 모드로 설정
 pipeline.model.eval()
 
-PROMPT = '''You are an AI doctor assistant which named '힐리' You have to advice about medical questions for your patients. The assistant gives helpful, detailed, and polite answers to the user's questions.\nHuman: {prompt}\nAssistant:\n'''
-instruction = "목감기에 걸렸는데 어느 병원으로 가야해?"
+PROMPT = '''Your name is "힐리", helping users with their medical concerns. The assistant gives helpful, detailed, proffesional and polite answers to the user's questions.'''
+instruction = input("증상을 말해주세요 ! : ")
+# instruction = "심각하게 목에 통증이 있는데 어느 병원으로 가야해?"
 
 messages = [
     {"role": "system", "content": f"{PROMPT}"},
@@ -51,18 +52,30 @@ terminators = [
     pipeline.tokenizer.eos_token_id,
     pipeline.tokenizer.convert_tokens_to_ids("<|eot_id|>")
 ]
+# 시간 측정 시작
+start_time = time.time()
 
 outputs = pipeline(
     prompt,
-    max_new_tokens=300,
+    max_new_tokens=400,
     eos_token_id=terminators,
     do_sample=True,
     temperature=0.7,
     top_p=0.92,
     top_k=50,
     num_return_sequences=1,
-    repetition_penalty=1.2,  # 반복 패널티 추가
-    no_repeat_ngram_size=3,  # 3-gram 반복 방지
+    repetition_penalty=5,  # 반복 패널티 추가
+    no_repeat_ngram_size=3,
+    num_beams = 2, # 3-gram 반복 방지
     early_stopping=True  # 조기 중단 활성화
 )
+
+# 시간 측정 종료
+end_time = time.time()
+
+# 경과 시간 계산
+elapsed_time = end_time - start_time
+
+print(f"답변 생성 시간: {elapsed_time:.2f}초")
+print(outputs[0]["generated_text"][len(prompt):])
 print(outputs[0]["generated_text"][len(prompt):])
